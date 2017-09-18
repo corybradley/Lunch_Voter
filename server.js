9930ec9ebeb256256ec7b328e3db25fe45490975
corybradley <cory.bradley@riskonnect.com>
2017-09-18 10:41:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const server = app/listen(8000, () => {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log('App listening at http://${host}:${port}`);
+  console.log('App listening at http://${host}:${port}`');
 });
